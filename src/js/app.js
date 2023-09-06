9510bcf8b171003907553c8f10f2658f38cca937
Florencia Barrios <florenciabarrios.n@gmail.com>
2023-09-06 21:13:50 +0200

    probando pruebas muestras de proyectos

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   darkMode();
   eventListeners();
   navegacionFija();
   scrollNav();
+  swiper();
+
 });
 
+function swiper() {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    allowTouchMove: true,
+    effect: "fade",
+    autoplay: {
+      delay: 4000,
+      pauseOnMouseEnter: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
 // Boton DARK-MODE
 function darkMode() {
   const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
@@ -27,7 +47,6 @@ function darkMode() {
   botonDarkMode.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
   });
-  
 }
 
 // Menu Hamburgesita
@@ -37,39 +56,38 @@ function eventListeners() {
 }
 
 function navegacionResponsive() {
-  const navegacion = document.querySelector('.navegacion');
-    navegacion.classList.toggle('mostrar');
+  const navegacion = document.querySelector(".navegacion");
+  navegacion.classList.toggle("mostrar");
 }
 
-// Header Fijo  
-function navegacionFija(){
-  const barra = document.querySelector('.header');
-  const resumenFlor = document.querySelector('.resumen-flor');
-  const body = document.querySelector('body');
+// Header Fijo
+function navegacionFija() {
+  const barra = document.querySelector(".header");
+  const resumenFlor = document.querySelector(".resumen-flor");
+  const body = document.querySelector("body");
 
-  window.addEventListener('scroll', function(){
-
-    if(resumenFlor.getBoundingClientRect().bottom < 0 ) {
-      barra.classList.add('fijo');
-      body.classList.add('body-scroll');
+  window.addEventListener("scroll", function () {
+    if (resumenFlor.getBoundingClientRect().bottom < 0) {
+      barra.classList.add("fijo");
+      body.classList.add("body-scroll");
     } else {
-      barra.classList.remove('fijo');
-      body.classList.remove('body-scroll');
+      barra.classList.remove("fijo");
+      body.classList.remove("body-scroll");
     }
   });
 }
 
 // Scroll Header
-function scrollNav(){
-  const enlaces = document.querySelectorAll('.boton-nav');
-  enlaces.forEach( enlace=> {
-    enlace.addEventListener('click', function(e){
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".boton-nav");
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", function (e) {
       e.preventDefault();
 
       const seccionScroll = e.target.attributes.href.value;
       const seccion = document.querySelector(seccionScroll);
-      seccion.scrollIntoView({ behavior: 'smooth' });
-    })
+      seccion.scrollIntoView({ behavior: "smooth" });
+    });
   });
 }
 
@@ -82,4 +100,37 @@ window.addEventListener("blur", () => {
 window.addEventListener("focus", () => {
   document.title = docTitle;
 });
+
+// 3º prueba de proyecto web
+// function swiper() {
+//   const swiper = new swiper(".swiper", {
+//     // Optional parameters
+//     direction: "horizontal",
+//     loop: true,
+//     allowTouchMove: true,
+//     effect: "fade",
+//     autoplay: {
+//       delay: 3000,
+//       pauseOnMouseEnter: true,
+//     },
+
+//     // If we need pagination
+//     // pagination: {
+//     //     el: '.swiper-pagination',
+//     // },
+
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+
+//     // And if we need scrollbar
+//     // scrollbar: {
+//     //     el: '.swiper-scrollbar',
+//     // },
+//   });
+// }
+
+
 
